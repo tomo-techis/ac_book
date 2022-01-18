063d34493f55693d58ab6d31720941f0f77d9f13
tomo-techis <t.ishioka.r@gmail.com>
2022-01-18 20:31:26 +0900

    home url変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/expense';
 
     /**
      * The controller namespace for the application.
